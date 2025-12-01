47cf5f6862092b54c7b757b1af0995e5bbaa9c1f
satvik6392 <87312565+satvik6392@users.noreply.github.com>
2025-12-01 19:56:16 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -118,7 +118,8 @@ app.post('/send-email', async (req, res) => {
   }
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
